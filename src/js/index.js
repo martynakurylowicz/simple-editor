01bf77c50a145830c5c46962de94c49b203c6840
Martyna Kuryłowicz <martynakurylowicz@gmail.com>
2020-06-13 23:07:12 +0200

    save in localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,19 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+const entryTextarea = document.querySelector(".newIdea--js");
+
+const saveButton = document.querySelector(".save--js");
+
+saveButton.addEventListener("click", () => {
+  localStorage.setItem("newIdea", entryTextarea.value);
+});
+
+const loadButton = document.querySelector(".load--js");
+
+loadButton.addEventListener("click", () => {
+  entryTextarea.value = localStorage.getItem("newIdea");
+});
